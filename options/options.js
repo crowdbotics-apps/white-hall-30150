881ec901bacd633924f238f76c70f056cccfc256
Crowdbotics <team@crowdbotics.com>
2021-08-27 16:35:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "white_hall_30150Identifier",
+  url: "https://white_hall_30150Identifier.botics.co",
+  api: "https://white_hall_30150Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
